0f5672d2d3a88aa0f8fe2e6bd1b85cf6076d8d60
Opilane TTHK <opilane@tthk.ee>
2023-02-13 13:34:37 +0200

    Initial commit

---
+++ ForEach/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nameList = new List<string>() { "Anton Tammsaare", "Lydia Koidula", "August Gailit", "Betti Alver" };
+            foreach (string name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("Vajuta 'Enter' et lahkuda..");
+        }
+    }
+}
